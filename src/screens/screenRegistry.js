ff61beaf9d2f132551815af212536099920ff171
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-14 21:00:04 -0400

    Define stable screen identifiers

---
+++ src/screens/screenRegistry.js
@@ -0,0 +1,8 @@
+export const SCREENS = Object.freeze({
+  PARTY: 'party',
+  HEROES: 'heroes',
+  VILLAGE: 'village',
+  ACHIEVEMENTS: 'achievements'
+});
+
+export const DEFAULT_SCREEN = SCREENS.PARTY;
