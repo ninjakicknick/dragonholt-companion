055c82c9ca1c97750af8423d9c427960454d37b9
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-14 20:58:19 -0400

    Add campaign state module entry point

---
+++ src/state/index.js
@@ -0,0 +1,4 @@
+export { useCampaign } from './useCampaign';
+export { useCampaignActions } from './useCampaignActions';
+export { createHeroDraft } from './useHeroActions';
+export { DEFAULT_STATE, SAVE_VERSION, STORAGE_KEY, importCampaign, loadCampaign, makeExport, migrateSave, normalizeCampaign, persistCampaign } from './campaignSave';
