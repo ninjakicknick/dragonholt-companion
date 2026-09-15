df5fb8761683a7a42fb0018c277754396663f61a
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-14 21:04:51 -0400

    Export campaign facade

--- src/state/index.js
+++ src/state/index.js
@@ -1,4 +1,5 @@
 export { useCampaign } from './useCampaign';
 export { useCampaignActions } from './useCampaignActions';
+export { useCampaignFacade } from './campaignFacade';
 export { createHeroDraft } from './useHeroActions';
 export { DEFAULT_STATE, SAVE_VERSION, STORAGE_KEY, importCampaign, loadCampaign, makeExport, migrateSave, normalizeCampaign, persistCampaign } from './campaignSave';
