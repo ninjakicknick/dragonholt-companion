0c01643bf30dfb9305f39056be80428e7c8a8e55
ninjakicknick <64557358+ninjakicknick@users.noreply.github.com>
2026-09-14 21:01:31 -0400

    Add screen metadata

---
+++ src/screens/screenMeta.js
@@ -0,0 +1,8 @@
+import { SCREENS } from './screenRegistry';
+
+export const SCREEN_META = {
+  [SCREENS.PARTY]: { label: 'Party' },
+  [SCREENS.HEROES]: { label: 'Heroes' },
+  [SCREENS.VILLAGE]: { label: 'Village' },
+  [SCREENS.ACHIEVEMENTS]: { label: 'Achievements' }
+};
